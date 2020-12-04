051fbfde05a7c7a23180a2c82c5949bfac1e579e
Theis Pedersen <theis@MacBook-Pro-tilhrende-Theis.local>
2020-12-04 17:24:50 +0100

    Database + Mongose + userroutes opsat

--- API/Routes/users.js
+++ API/Routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
 
-
+const User = require("../models/user.js") // user klassen/skema importeres fra fra user-modelfilen
 
 
 //importerede controllerFunctions:
@@ -12,17 +13,46 @@ const router = express.Router();
 
 // nedenstående skal bare være ("/") fordi linjen: app.use("/users",userRoutes); henviser alle requests om users/users endpointet i url'en henvises  til denne fil go ska lderfor ikke henvises videre da denne controller er "sidste stop" -> tager sig af hvor der skal hentes/sendes
 
+// overvej om denne funktion skal specificeres så der kan findes alle kvindelige eller alle mandlige bruger udelukkende
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Handling GET requests to /Users"
+   User.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+        if (docs.length >= 0) {
+            res.status(200).json()
+        } else {
+            res.status(404).json({
+                message: "No User entries found"
+            });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.post("/", (req, res, next) => {
-    const user = { 
-        name: req.body.name,
-        password: req.body.password
-    };
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        gender: req.body.gender,
+        age: req.body.age,
+        interests: req.body.interests
+    });
+    user
+        .save()
+        .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
     res.status(200).json({
         message: "Handling POST requests to /Post",
         createdUser: user
@@ -32,32 +62,60 @@ router.post("/", (req, res, next) => {
 
 
 // kolon efterfulgt af id
-router.get("/:username", (req, res, next) => {
-    const id = req.params.username;
-    if (id === "Theis1997") {
-        res.status(200).json({
-            message: "You logged into user: Theis1997",
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: "You passed an other ID than Theis1997"
+router.get("/:userId", (req, res, next) => {
+    const id = req.params.userId;
+    User.findById(id)
+      .exec()
+      .then(doc => {
+        console.log("From database", doc);
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res
+            .status(404)
+            .json({ message: "No valid user found corresponding with provided ID" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+      });
+  });
 
-        });
+router.patch("/:userId", (req, res, next) => {
+    const id = req.params.userId;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
     }
-
-});
-
-router.patch("/:username", (req, res, next) => {
-    res.status(200).json({
-        message: "Updated user!"
+    User.update({_id: id}, {$set: updateOps}) //der ændres på objektet der refereres til med et givent id (ændres kun på objekter der opfylder id-kriteriet) . Her bruges propertyen $set, (mongoose) til at ændre json-objektet ud fra det value/key-set som er defineret i objektets schema/klasse //Eller som i nuværende kode: js-Objektet updateOps oprettes og der loopes igennem alle req.body operations -  req.body opfattes som array
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
+// Der kan med denne metode patches enkelte values af gangen i stedet for at patche alle values
 
 
-router.delete("/:username", (req, res, next) => {
-    res.status(200).json({
-        message: "Deleted product!"
+router.delete("/:userId", (req, res, next) => {
+    const id = req.params.userId;
+    User.remove({_id: id})
+    .exec()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
@@ -76,3 +134,8 @@ module.exports = router;
 //}
 
 //module.exports = userController; 
+
+
+
+
+
--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require("express");
 var app = express();
 const PORT = 3001;
 var bodyParser = require("body-parser"); // Bruges til at parse bodies kan båe URL og JSON format
+const mongoose = require("mongoose");
 
 
 const userRouter = require("./API/Routes/users");
 const matchRouter = require("./API/Routes/match");
 
+mongoose.connect("mongodb+srv://node-Datingapp:chukwudubem97@cluster0.moqel.mongodb.net/DatingAppEX1?retryWrites=true&w=majority", {
+
+useNewUrlParser: true,
+useUnifiedTopology: true}
+); 
 
 
 app.use(bodyParser.urlencoded({extended: false})); // extended er her false fordi vi ikke har brug for at sende extended bodies med rich data (true) -> slå rich data op 
@@ -14,16 +20,17 @@ app.use(bodyParser.json());
 
 //Rigtige headers sendes til browseren (inden koden når til routes -> der sendes requests) -> så browseren der kører på en anden PORT/local host tillades adgang til serveren
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Acess-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "*"
     );
     if (req.method === "OPTIONS") {
-        res.header("Acces-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-       return res.status(200).json({});
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
     }
     next();
-});
+  });
 
 app.use("/Users",userRouter); 
 app.use("/Match",matchRouter);
@@ -49,4 +56,11 @@ app.use((error, req, res, next) => {
 
 
 app.listen(PORT); 
-console.log("Server is active and listening on port 3001 - yay!");
+console.log("SERVER IS ACTIVE AND LISTENING ON PORT 3001 - YAY!");
+
+
+
+
+
+
+
